adf3cda1b307bfaa49574feb768d78c5a37bef3e
avan_web_okbet <avansoftify@gmail.com>
2024-09-09 18:22:13 +0800

    formatted

--- src/page/LoadingPage/index.tsx
+++ src/page/LoadingPage/index.tsx
@@ -2,21 +2,17 @@ import clsx from "clsx";
 import style from "./index.module.scss";
 import LogoImage from "../../component/logoComp";
 
-
 const Loading = () => {
   return (
     <div className={clsx(style.loading)}>
-     <LogoImage></LogoImage>
+      <LogoImage></LogoImage>
 
-     <div className={clsx(style.loadingSection)}>
+      <div className={clsx(style.loadingSection)}>
         <div className={clsx(style.loadingBar)}>
-            <div className={clsx(style.progress)}>
-
-            </div>
+          <div className={clsx(style.progress)}></div>
         </div>
         <span>Loading....</span>
-  
-     </div>
+      </div>
     </div>
   );
 };
